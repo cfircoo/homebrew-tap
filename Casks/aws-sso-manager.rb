30395f94eeb3e5d40dbb93a747b36980ac574e4e
Cfir Carmeli <cfir@carmeli.me>
2025-09-06 07:49:11 +0300

    Fix filename to match actual release: AWS.SSO.Manager-2.0.0-arm64.dmg

--- Casks/aws-sso-manager.rb
+++ Casks/aws-sso-manager.rb
@@ -2,11 +2,11 @@ cask "aws-sso-manager" do
   version "2.0.0"
   sha256 "b2975a9a906dc9fe0218f87afc05bb4b346a3a376000dbf1c0e054a797154f39"
 
-  url "https://github.com/cfir-sensi/aws-sso-manager/releases/download/v#{version}/AWS%20SSO%20Manager-#{version}-arm64.dmg",
-      verified: "github.com/cfir-sensi/aws-sso-manager/"
+  url "https://github.com/cfircoo/aws-sso-manager/releases/download/v#{version}/AWS.SSO.Manager-#{version}-arm64.dmg",
+      verified: "github.com/cfircoo/aws-sso-manager/"
   name "AWS SSO Manager"
   desc "Desktop application that simplifies AWS Single Sign-On session management and role switching"
-  homepage "https://github.com/cfir-sensi/aws-sso-manager"
+  homepage "https://github.com/cfircoo/aws-sso-manager"
 
   app "AWS SSO Manager.app"
 
